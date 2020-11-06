d8734df902d5e60084e416025d811c1c0a716ebb
Mehtap Isik <misik@lilac.hpc.private>
2020-11-06 16:52:18 -0500

    Update t1 of dry_octanol_GAFF_tip3p

---
+++ dry_octanol_GAFF_tip3p/dry_runs/extract-dcd/convert_t2_nc_trajectories_to_dcd.sh
@@ -0,0 +1,30 @@
+# Activate environment
+source activate SAMPL6_logP
+
+# Create output directory for dcd files
+    output_directory_name="t2_dcd_files"
+    echo "$output_directory_name"
+    mkdir "$output_directory_name"
+
+# Create dcd file for each phase of each trajectoru
+for d in ../t2/dry_yank_output/* ; do
+    echo "$d"
+
+    # Extract system name
+    system_name="$(cut -d'/' -f4 <<<"$d")"
+    echo "$system_name"
+    path_to_output_system_directory="$output_directory_name/$system_name"
+
+    # Create system subdirectory in output directory and move dcd files to output direct
+    mkdir "$path_to_output_system_directory"
+
+    # Convert nc to dcd
+    yank analyze extract-trajectory --trajectory="$path_to_output_system_directory/anti_trial2_solvent1.dcd" --netcdf="$d/solvent1.nc" --state=0
+    yank analyze extract-trajectory --trajectory="$path_to_output_system_directory/anti_trial2_solvent2.dcd" --netcdf="$d/solvent2.nc" --state=0
+
+done
+
+# Create tar.gz bundle of the output directory
+tar -czvf "$output_directory_name.tar.gz" "$output_directory_name"
+
+echo "Done!"
